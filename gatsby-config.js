ba80a81964c23457bed6c40109ee391aafb39a88
Yusong Shi <jackshi.sys@gmail.com>
2022-10-12 17:27:53 -0700

    setup

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+};
+
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
+}
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  );
+}
+
+module.exports = {
+  siteMetadata: {
+    title: `Portfolio V2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1 className="text-red-800">Hello World</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
